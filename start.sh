7d1c35cc2db8f4be2f747b5cbe5df253e3db128c
MShibli <101069136+MShibli@users.noreply.github.com>
2025-12-21 15:05:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ echo "Starte Task 1"
 python -m ebAlert links -a "https://www.kleinanzeigen.de/s-pc-zubehoer-software/speicher/c225+pc_zubehoer_software.art_s:speicher"
 
 echo "Starte Task 2"
-python -m ebAlert start --config config2.yml
+python -m ebAlert start
 
 echo "Alle Tasks fertig"
