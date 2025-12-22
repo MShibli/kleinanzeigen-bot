ecc632f8b3a5ea761b1650780e1faf3c9fe04ad2
MShibli <101069136+MShibli@users.noreply.github.com>
2025-12-22 01:11:58 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Starte Task 1"
 python -m ebAlert links -a "https://www.kleinanzeigen.de/s-pc-zubehoer-software/grafikkarten,mainboards,prozessor_cpu,sonstiges,speicher/c225+pc_zubehoer_software.art_s:(grafikkarten%2Cmainboards%2Cprozessor_cpu%2Csonstiges%2Cspeicher)"
+python -m ebAlert links -a "https://www.kleinanzeigen.de/s-handy-telekom/preis::420/c173"
 echo "Starte Task 2"
 python -m ebAlert start
 
