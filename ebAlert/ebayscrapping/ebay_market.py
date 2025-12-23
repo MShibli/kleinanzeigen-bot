7024eeb8cb0d938a007378b815ccb7d0f4f99470
MShibli <101069136+MShibli@users.noreply.github.com>
2025-12-23 15:52:54 +0100

    Update ebay_market.py

--- ebAlert/ebayscrapping/ebay_market.py
+++ ebAlert/ebayscrapping/ebay_market.py
@@ -49,7 +49,7 @@ def get_ebay_median_price(query: str, offer_price: float):
     try:
         res = requests.get(url, headers=headers, timeout=10)
         raw_matches = re.findall(r"EUR\s?(\d+(?:\.\d+)?,\d{2})", res.text)
-        
+        print(f"ebay Scrap Text: {res.Text}")
         all_prices = []
         min_gate = offer_price * 0.5
         max_gate = offer_price * 2.0
